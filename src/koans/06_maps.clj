59fd5f8f26836e034839f396504932a4e35bdab9
Josh Cheek <josh.cheek@gmail.com>
2017-05-06 15:03:18 -0500

    Reset files for walkthrough

--- src/koans/06_maps.clj
+++ src/koans/06_maps.clj
@@ -3,60 +3,60 @@
 
 (meditations
   "Don't get lost when creating a map"
-  (= {:a 1 :b 2} (hash-map :a 1 :b 2))
+  (= {:a 1 :b 2} (hash-map :a 1 __ __))
 
   "A value must be supplied for each key"
-  (= {:a 1} (hash-map :a 1))
+  (= {:a 1} (hash-map :a __))
 
   "The size is the number of entries"
-  (= 2 (count {:a 1 :b 2}))
+  (= __ (count {:a 1 :b 2}))
 
   "You can look up the value for a given key"
-  (= 2 (get {:a 1 :b 2} :b))
+  (= __ (get {:a 1 :b 2} :b))
 
   "Maps can be used as functions to do lookups"
-  (= 1 ({:a 1 :b 2} :a))
+  (= __ ({:a 1 :b 2} :a))
 
   "And so can keywords"
-  (= 1 (:a {:a 1 :b 2}))
+  (= __ (:a {:a 1 :b 2}))
 
   "But map keys need not be keywords"
-  (= "Sochi" ({2010 "Vancouver" 2014 "Sochi" 2018 "PyeongChang"} 2014))
+  (= __ ({2010 "Vancouver" 2014 "Sochi" 2018 "PyeongChang"} 2014))
 
   "You may not be able to find an entry for a key"
-  (= nil (get {:a 1 :b 2} :c))
+  (= __ (get {:a 1 :b 2} :c))
 
   "But you can provide your own default"
-  (= :key-not-found (get {:a 1 :b 2} :c :key-not-found))
+  (= __ (get {:a 1 :b 2} :c :key-not-found))
 
   "You can find out if a key is present"
-  (= true (contains? {:a nil :b nil} :b))
+  (= __ (contains? {:a nil :b nil} :b))
 
   "Or if it is missing"
-  (= false (contains? {:a nil :b nil} :c))
+  (= __ (contains? {:a nil :b nil} :c))
 
   "Maps are immutable, but you can create a new and improved version"
-  (= {1 "January" 2 "February"} (assoc {1 "January"} 2 "February"))
+  (= {1 "January" 2 __} (assoc {1 "January"} 2 "February"))
 
   "You can also create a new version with an entry removed"
-  (= {1 "January"} (dissoc {1 "January" 2 "February"} 2))
+  (= {__ __} (dissoc {1 "January" 2 "February"} 2))
 
   "Create a new map by merging"
-  (= {:a 1 :b 2 :c 3} (merge {:a 1 :b 2} {:c 3}))
+  (= {:a 1 :b 2 __ __} (merge {:a 1 :b 2} {:c 3}))
 
   "Specify how to handle entries with same keys when merging"
-  (= {:a 1 :b 2 :c 3} (merge-with + {:a 1 :b 1} {:b 1 :c 3}))
+  (= {:a 1 :b __ :c 3} (merge-with + {:a 1 :b 1} {:b 1 :c 3}))
 
   "Often you will need to get the keys, but the order is undependable"
-  (= (list 2010 2014 2018)
+  (= (list __ __ __)
      (sort (keys { 2014 "Sochi" 2018 "PyeongChang" 2010 "Vancouver"})))
 
   "You can get the values in a similar way"
-  (= (list "PyeongChang" "Sochi" "Vancouver")
+  (= (list __ __ __)
      (sort (vals {2010 "Vancouver" 2014 "Sochi" 2018 "PyeongChang"})))
 
   "You can even iterate over the map entries as a seq"
-  (= {:a 2 :b 3}
+  (= {:a __ :b __}
      (into {}
            (map
             (fn [[k v]] [k (inc v)])
--- src/koans/08_conditionals.clj
+++ src/koans/08_conditionals.clj
@@ -10,38 +10,38 @@
 
 (meditations
   "You will face many decisions"
-  (= :a (if (false? (= 4 5))
+  (= __ (if (false? (= 4 5))
           :a
           :b))
 
   "Some of them leave you no alternative"
-  (= [] (if (> 4 3)
+  (= __ (if (> 4 3)
           []))
 
   "And in such a situation you may have nothing"
-  (= nil (if (nil? 0)
+  (= __ (if (nil? 0)
           [:a :b :c]))
 
   "In others your alternative may be interesting"
   (= :glory (if (not (empty? ()))
               :doom
-              :glory))
+              __))
 
   "You may have a multitude of possible paths"
   (let [x 5]
-    (= :your-road (cond (= x 1) :road-not-taken
-                        (= x 2) :another-road-not-taken
-                        :else :your-road)))
+    (= :your-road (cond (= x __) :road-not-taken
+                        (= x __) :another-road-not-taken
+                        :else __)))
 
   "Or your fate may be sealed"
-  (= 'doom (if-not (zero? 1)
+  (= 'doom (if-not (zero? __)
           'doom
           'more-doom))
 
   "In case of emergency, go fast"
   (= "pretty fast"
-     (explain-exercise-velocity :bicycling))
+     (explain-exercise-velocity __))
 
   "But admit it when you don't know what to do"
-  (= "is that even exercise?"
+  (= __
      (explain-exercise-velocity :watching-tv)))
